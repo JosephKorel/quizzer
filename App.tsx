6d6a6e35a2d3edcd6f118da39f6ccea8cabda9f3
José Carlos Rolim Etchepare <jcjosecarlos03@gmail.com>
2022-07-11 10:13:39 -0300

    react navigation

--- App.tsx
+++ App.tsx
@@ -1,41 +1,17 @@
 import { useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, View } from "react-native";
-import * as Google from "expo-auth-session/providers/google";
-import { GoogleAuthProvider, signInWithCredential } from "firebase/auth";
-import { auth } from "./firebase_config";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Login from "./Screens/login";
+
+const { Navigator, Screen } = createNativeStackNavigator();
 
 export default function App() {
-  const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
-    clientId:
-      "494904537547-jmd1np75cd9dab6cla7jculb9gketsre.apps.googleusercontent.com",
-  });
-
-  useEffect(() => {
-    if (response?.type === "success") {
-      const { id_token } = response.params;
-      const credential = GoogleAuthProvider.credential(id_token);
-      signInWithCredential(auth, credential)
-        .then((res) => {
-          console.log("Success");
-        })
-        .catch((err) => console.log(err));
-    }
-  }, [response]);
-
   return (
-    <View style={styles.container}>
-      <Text>Entre com sua conta do Google</Text>
-      <Button onPress={() => promptAsync()} title="Entrar"></Button>
-    </View>
+    <NavigationContainer>
+      <Navigator initialRouteName="Login">
+        <Screen name="Login" component={Login}></Screen>
+      </Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ Screens/login.tsx
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import * as Google from "expo-auth-session/providers/google";
+import { GoogleAuthProvider, signInWithCredential } from "firebase/auth";
+import { auth } from "../firebase_config";
+
+export default function Login() {
+  const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
+    clientId:
+      "494904537547-jmd1np75cd9dab6cla7jculb9gketsre.apps.googleusercontent.com",
+  });
+
+  useEffect(() => {
+    if (response?.type === "success") {
+      const { id_token } = response.params;
+      const credential = GoogleAuthProvider.credential(id_token);
+      signInWithCredential(auth, credential)
+        .then((res) => {
+          console.log("Success");
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [response]);
+
+  return (
+    <View>
+      <Text>Entre com sua conta do Google</Text>
+      <Button onPress={() => promptAsync()} title="Entrar"></Button>
+    </View>
+  );
+}
